d3edbeda0a63ca8567ce4f31bfdde82fef08e35d
Peter Kováč <peteris.kovac@gmail.com>
2023-12-11 13:32:30 +0100

    Class World upgraded

--- src/main/java/org/gfa/avustribesbackend/models/World.java
+++ src/main/java/org/gfa/avustribesbackend/models/World.java
@@ -2,42 +2,68 @@ package org.gfa.avustribesbackend.models;
 
 import jakarta.persistence.*;
 
+import java.util.Random;
+
 @Entity
 @Table(name = "worlds")
 public class World {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
-    private Long Id;
+    private Long id;
     @Column(unique = true, nullable = false)
-    private String Name;
+    private String name;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "world_player",
-            joinColumns = @JoinColumn(name = "world_id"),
-            inverseJoinColumns = @JoinColumn(name = "player_id"))
-    private List<Player> players;
+//    @ManyToMany(cascade = CascadeType.ALL)
+//    @JoinTable(name = "world_player",
+//            joinColumns = @JoinColumn(name = "world_id"),
+//            inverseJoinColumns = @JoinColumn(name = "player_id"))
+//    private List<Player> players;
 
     public World(String name) {
-        Name = name;
+        this.name = name;
     }
 
+
     public World() {
+        name = generatorOfName();
+    }
+
+    public String generatorOfName(){
+        Random random  = new Random();
+
+        String[] StartOfName = {"Al", "Gr", "Han", "Poer", "Greg", "Ion", "Qer", "Sam",
+        "Nup", "Res", "Clar", "Sour", "Wer", "Has", "Kil", "Dus", "Fux", "Jadus", "Taur"};
+
+        String[] EndOfName = {"dor", "ion", "ian", "as", "or", "nor", "er", "ef", "fu",
+        "gu", "vunu", "op", "nin"};
+
+
+        return StartOfName[random.nextInt(StartOfName.length)] +
+                EndOfName[random.nextInt(EndOfName.length)];
     }
 
     public Long getId() {
-        return Id;
+        return id;
     }
 
     public void setId(Long id) {
-        Id = id;
+        this.id = id;
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
     public void setName(String name) {
-        Name = name;
+        this.name = name;
     }
+
+//    public List<Player> getPlayers() {
+//        return players;
+//    }
+//
+//    public void setPlayers(List<Player> players) {
+//        this.players = players;
+//    }
 }

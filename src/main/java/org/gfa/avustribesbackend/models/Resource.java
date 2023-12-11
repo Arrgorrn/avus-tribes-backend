09b5712803adbb3ddc3c51f0966712e415de84ca
Gerzson Pszota <gerzson.pszota@gmail.com>
2023-12-11 11:07:59 +0100

    <feat>(): <added building and resource entity>

---
+++ src/main/java/org/gfa/avustribesbackend/models/Resource.java
@@ -0,0 +1,14 @@
+package org.gfa.avustribesbackend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Resource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}

c14f1b4f944f6671f67ec8e1f3cfe2c70bfa17b2
Peter Kováč <peteris.kovac@gmail.com>
2023-12-12 13:53:18 +0100

    <type>(fix): <Properties fix>

--- src/main/java/org/gfa/avustribesbackend/repositories/WorldRepository.java
+++ src/main/java/org/gfa/avustribesbackend/repositories/WorldRepository.java
@@ -2,6 +2,8 @@ package org.gfa.avustribesbackend.repositories;
 
 import org.gfa.avustribesbackend.models.World;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WorldRepository extends JpaRepository<World, Long> {
 }

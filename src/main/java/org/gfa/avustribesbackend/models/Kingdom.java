0b0674a827ba1ae9e69c05a430d9ae36c6843b36
Karolína Mráčková <139132541+karolinamrackova@users.noreply.github.com>
2023-12-13 09:17:02 +0100

    Atb 3 (#1)
    * <feat>(): added kingdom entity>
    
    * <feat>(): <add relationships and tests>
    
    * <fix>: <env file should not be committed anymore>
    
    * <fix>: <env file>
    
    * <fix>(): <add Kingdom Repository, delete unnecessary dependencies>
    
    * <fix>(): <minor fixes>
    
    * <fix>(): <fixed db source>
    
    * <fix>(): <fixes from barna's comments>
    
    * fix: add .env.sample file

--- src/main/java/org/gfa/avustribesbackend/models/Kingdom.java
+++ src/main/java/org/gfa/avustribesbackend/models/Kingdom.java
@@ -1,14 +1,76 @@
 package org.gfa.avustribesbackend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Kingdom {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "coordinate_x")
+    private Double coordinateX;
+
+    @Column(name = "coordinate_y")
+    private Double coordinateY;
+
+ /*   @ManyToOne
+    @JoinColumn(name = "world_id", nullable = false)
+    private World world;*/
+
+    @OneToMany(mappedBy = "kingdom")
+    private List<Production> productions;
+
+
+    public Kingdom() {
+    }
+
+    public Kingdom(String name, Double coordinateX, Double coordinateY) {
+        this.name = name;
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+    }
+
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCoordinateX() {
+        return coordinateX;
+    }
+
+    public void setCoordinateX(Double coordinateX) {
+        this.coordinateX = coordinateX;
+    }
+
+    public Double getCoordinateY() {
+        return coordinateY;
+    }
+
+    public void setCoordinateY(Double coordinateY) {
+        this.coordinateY = coordinateY;
+    }
+
 }

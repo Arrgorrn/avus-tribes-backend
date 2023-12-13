f991ddbd0feba9e5f79c812175e57e8dc957395d
Gerzson Pszota <gerzson.pszota@gmail.com>
2023-12-13 10:34:46 +0100

    feat: adding Resource entity, repo

--- src/main/java/org/gfa/avustribesbackend/models/Resource.java
+++ src/main/java/org/gfa/avustribesbackend/models/Resource.java
@@ -11,4 +11,56 @@ public class Resource {
 
     @OneToOne(mappedBy = "resource")
     private Production production;
+
+    @ManyToOne
+    @JoinColumn(name = "kingdom_id", nullable = false)
+    private Kingdom kingdom;
+
+    private int amount;
+
+  public Resource() {
+    this.amount = 0;
+  }
+
+  public Resource(Long id, Production production, Kingdom kingdom) {
+    this.id = id;
+    this.production = production;
+    this.kingdom = kingdom;
+    this.amount = 0;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Production getProduction() {
+    return production;
+  }
+
+  public void setProduction(Production production) {
+    this.production = production;
+  }
+
+  public Kingdom getKingdom() {
+    return kingdom;
+  }
+
+  public void setKingdom(Kingdom kingdom) {
+    this.kingdom = kingdom;
+  }
+
+  public int getAmount() {
+    return amount;
+  }
+
+  public void setAmount(int amount) {
+    if (amount < 0) {
+      throw new IllegalArgumentException("Amount cannot be negative");
+    }
+    this.amount = amount;
+  }
 }

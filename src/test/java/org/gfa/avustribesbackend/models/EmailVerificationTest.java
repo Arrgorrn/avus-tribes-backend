70549e06cfc72995d8b5463fbd17ce1e08f77459
Karolína Mráčková <karolinka-m@seznam.cz>
2023-12-14 16:10:06 +0100

    feat email verification: add test class, waiting for registration

---
+++ src/test/java/org/gfa/avustribesbackend/models/EmailVerificationTest.java
@@ -0,0 +1,35 @@
+package org.gfa.avustribesbackend.models;
+
+import org.junit.jupiter.api.Test;
+
+public class EmailVerificationTest {
+
+    @Test
+    public void email_gets_sent_to_player(){
+
+    }
+
+    @Test
+    public void verification_successful(){
+
+
+    }
+
+    @Test
+    public void verification_unsuccessful_expired_token(){
+
+
+    }
+
+    @Test
+    public void token_expired_generate_new_token_and_verify(){
+
+
+
+    }
+
+
+    
+
+
+}

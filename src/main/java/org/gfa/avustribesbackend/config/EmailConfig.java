d865ee86f77362ae9e874a79f731499c1c47c853
Karolína Mráčková <karolinka-m@seznam.cz>
2024-01-10 14:22:31 +0100

    fix: email sending is working!

--- src/main/java/org/gfa/avustribesbackend/config/EmailConfig.java
+++ src/main/java/org/gfa/avustribesbackend/config/EmailConfig.java
@@ -1,32 +1,22 @@
 package org.gfa.avustribesbackend.config;
 
-import java.util.Properties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-@Configuration
-public class EmailConfig {
+  @Configuration
+  public class EmailConfig {
 
     @Bean
     public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("sandbox.smtp.mailtrap.io");
-        mailSender.setPort(25);
-        mailSender.setUsername("7a747e823a1cea");
-        mailSender.setPassword("3ef05efea69199");
+      JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+      mailSender.setHost("sandbox.smtp.mailtrap.io");
+      mailSender.setPort(25);
+      mailSender.setUsername("9df1100280b9f2");
+      mailSender.setPassword("36f6cbde27134b");
 
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.smtp.socketFactory.port", "25");
-//        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-//        props.put("mail.smtp.socketFactory.fallback", "false");
-        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
-//        props.put("mail.smtp.ssl.enable", "false");
-        props.put("mail.debug", "true");
-
-        return mailSender;
+      return mailSender;
     }
-}
+  }
+

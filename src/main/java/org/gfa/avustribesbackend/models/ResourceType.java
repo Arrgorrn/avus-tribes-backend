2c03f756f31d573c60a539b2f1313325100e154a
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-01-16 14:42:46 +0100

    feat: adding constructors, getters/setters

--- src/main/java/org/gfa/avustribesbackend/models/ResourceType.java
+++ src/main/java/org/gfa/avustribesbackend/models/ResourceType.java
@@ -12,4 +12,26 @@ public class ResourceType {
 
   @OneToOne(mappedBy = "resourceType", cascade = CascadeType.ALL)
   private BuildingType buildingType;
+
+  public ResourceType() {}
+
+  public ResourceType(BuildingType buildingType) {
+    this.buildingType = buildingType;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public BuildingType getBuildingType() {
+    return buildingType;
+  }
+
+  public void setBuildingType(BuildingType buildingType) {
+    this.buildingType = buildingType;
+  }
 }

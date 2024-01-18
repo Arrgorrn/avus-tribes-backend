bea23f3aa63fb63d1af879f5c18435b1a89957b9
arrgorrn <michal.filakk@gmail.com>
2024-01-18 12:44:07 +0100

    feat: add logging interceptror ang logging

--- src/main/java/org/gfa/avustribesbackend/exceptions/PlayerExceptionHandler.java
+++ src/main/java/org/gfa/avustribesbackend/exceptions/PlayerExceptionHandler.java
@@ -1,6 +1,9 @@
 package org.gfa.avustribesbackend.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.gfa.avustribesbackend.components.RequestResponseLoggingInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,6 +14,9 @@ import java.util.Date;
 @RestControllerAdvice
 public class PlayerExceptionHandler {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(RequestResponseLoggingInterceptor.class);
+
+
   @ExceptionHandler({
       CredentialException.class,
       VerificationException.class,
@@ -20,6 +26,9 @@ public class PlayerExceptionHandler {
   public ErrorResponse badRequestExceptionHandler(
       RuntimeException runtimeException,
       HttpServletRequest httpServletRequest) {
+    LOGGER.error("ERROR: " + runtimeException.getMessage());
+    LOGGER.error("Status code: {}", HttpStatus.BAD_REQUEST);
+    LOGGER.info("<=======================================================Response end========================================================>");
     return new ErrorResponse(
         runtimeException.getMessage(),
         httpServletRequest.getRequestURI(),
@@ -31,6 +40,9 @@ public class PlayerExceptionHandler {
   public ErrorResponse conflictExceptionHandler(
       RuntimeException runtimeException,
       HttpServletRequest httpServletRequest) {
+    LOGGER.error("ERROR: " + runtimeException.getMessage());
+    LOGGER.error("Status code: {}", HttpStatus.CONFLICT);
+    LOGGER.info("<=======================================================Response end========================================================>");
     return new ErrorResponse(
         runtimeException.getMessage(),
         httpServletRequest.getRequestURI(),

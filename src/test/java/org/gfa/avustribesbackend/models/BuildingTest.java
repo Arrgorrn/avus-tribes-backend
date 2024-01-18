6ed11a1d4fdded926fe39f0ab0449c07abc29003
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-01-18 12:08:51 +0100

    refactor: tests according to google java format

--- src/test/java/org/gfa/avustribesbackend/models/BuildingTest.java
+++ src/test/java/org/gfa/avustribesbackend/models/BuildingTest.java
@@ -3,8 +3,6 @@ package org.gfa.avustribesbackend.models;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuildingTest {

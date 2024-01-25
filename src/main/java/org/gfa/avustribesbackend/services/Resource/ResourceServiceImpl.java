42895587b8dac7cdeb41a3472bc12f31e2bf712b
Peter Kováč <peteris.kovac@gmail.com>
2024-01-25 14:17:01 +0100

    feat : time service + method

---
+++ src/main/java/org/gfa/avustribesbackend/services/Resource/ResourceServiceImpl.java
@@ -0,0 +1,36 @@
+package org.gfa.avustribesbackend.services.Resource;
+
+import org.gfa.avustribesbackend.models.Building;
+import org.gfa.avustribesbackend.models.Resource;
+import org.gfa.avustribesbackend.repositories.ResourceRepository;
+import org.gfa.avustribesbackend.services.Time.TimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResourceServiceImpl implements TimeService {
+  private final ResourceRepository resourceRepository;
+
+  @Autowired
+  public ResourceServiceImpl(ResourceRepository resourceRepository) {
+    this.resourceRepository = resourceRepository;
+  }
+
+  @Override
+  @Scheduled(fixedRate = 60000)
+  public void gainingResources() {
+    List<Resource> resources = resourceRepository.findAll();
+    for (Resource resource : resources) {
+      for (Building building : resource.getResourceType().getBuildingType().getBuildings()) {
+        if (building.getLevel() == 1) {
+          resource.setAmount(resource.getAmount() + 10);
+        } else {
+          resource.setAmount(resource.getAmount() + (10 + (5 * building.getLevel())));
+        }
+      }
+    }
+  }
+}
--- src/main/java/org/gfa/avustribesbackend/services/Time/TimeService.java
+++ src/main/java/org/gfa/avustribesbackend/services/Time/TimeService.java
@@ -1,8 +1,5 @@
 package org.gfa.avustribesbackend.services.Time;
 
-import java.time.LocalDateTime;
-
 public interface TimeService {
-  LocalDateTime getCurrentTime();
-  long calculateElapsedTime(LocalDateTime startTime, LocalDateTime endTime);
+  void gainingResources();
 }

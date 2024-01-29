18786a4fb813aaa48340dec9897e4ddee5748b02
arrgorrn <michal.filakk@gmail.com>
2024-01-29 10:23:33 +0100

    fix: fix indentation

--- src/main/java/org/gfa/avustribesbackend/services/JWT/JwtService.java
+++ src/main/java/org/gfa/avustribesbackend/services/JWT/JwtService.java
@@ -9,8 +9,12 @@ import java.util.function.Function;
 public interface JwtService {
 
   String extractUsername(String token);
+
   <T> T extractClaim(String token, Function<Claims, T> claimsResolver);
+
   String generateToken(UserDetails userDetails);
+
   String generateToken(Map<String, Object> extraClaims, UserDetails userDetails);
+
   boolean isTokenValid(String token, UserDetails userDetails);
 }

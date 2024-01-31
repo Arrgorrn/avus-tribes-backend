c935aa5b7711564501543ec9b85ef0e61e51d098
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-01-31 10:40:37 +0100

    Atb 27 - Updating buildings (#25)
    * feat: create starting kingdom after player verification
    
    * feat: buildingexception added
    
    * feat: upgrade building
    
    * fix: townhall is now excluded from maxAllowerdLevel-check
    
    * fix: getBuildingLevel fixed
    
    * fix: removed unused import
    
    * refactor: simplifying maxAllowedLevel
    
    * feat: saving to repo
    
    * fix: readability
    
    ---------
    
    Co-authored-by: Karolína Mráčková <karolinka-m@seznam.cz>

---
+++ src/main/java/org/gfa/avustribesbackend/services/Building/BuildingService.java
@@ -0,0 +1,9 @@
+package org.gfa.avustribesbackend.services.Building;
+
+import org.gfa.avustribesbackend.models.Kingdom;
+import org.gfa.avustribesbackend.models.Resource;
+import org.gfa.avustribesbackend.models.enums.BuildingTypeValue;
+
+public interface BuildingService {
+  void upgradeBuilding(Kingdom kingdom, BuildingTypeValue buildingType, Resource gold);
+}

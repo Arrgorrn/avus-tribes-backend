97de1af65a37b36bbf95c4669a319e5e976537b4
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-02-01 13:22:24 +0100

    feat: upgradeBuilding

--- src/main/java/org/gfa/avustribesbackend/controllers/BuildingController.java
+++ src/main/java/org/gfa/avustribesbackend/controllers/BuildingController.java
@@ -1,9 +1,16 @@
 package org.gfa.avustribesbackend.controllers;
 
+import org.gfa.avustribesbackend.dtos.UpgradeBuildingDTO;
+import org.gfa.avustribesbackend.exceptions.BuildingException;
+import org.gfa.avustribesbackend.models.Kingdom;
+import org.gfa.avustribesbackend.models.Resource;
+import org.gfa.avustribesbackend.models.enums.BuildingTypeValue;
 import org.gfa.avustribesbackend.services.Building.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +24,21 @@ public class BuildingController {
   }
 
   @PatchMapping("/kingdoms/upgrade")
-  public ResponseEntity<Object> upgradeBuilding() {
+  public ResponseEntity<Object> upgradeBuilding(
+      @RequestBody UpgradeBuildingDTO upgradeBuildingDTO) {
+    try {
+      Kingdom kingdom = upgradeBuildingDTO.getKingdom();
+      BuildingTypeValue buildingType = upgradeBuildingDTO.getBuildingType();
 
+      buildingService.upgradeBuilding(kingdom, buildingType);
+
+      int upgradeTime = buildingService.getBuildingLevel(kingdom, buildingType);
+      //add logic here to delay with upgradeTime minutes
+
+      return ResponseEntity.ok(
+          "Building upgrade started. Time required: " + upgradeTime + " minutes.");
+    } catch (BuildingException be) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(be.getMessage());
+    }
   }
 }

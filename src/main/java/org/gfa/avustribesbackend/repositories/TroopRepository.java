d949adbc6d70b3091881a77f7f6ada9ddad3183a
Peter Kováč <peteris.kovac@gmail.com>
2024-02-06 14:17:26 +0100

    fix : format

--- src/main/java/org/gfa/avustribesbackend/repositories/TroopRepository.java
+++ src/main/java/org/gfa/avustribesbackend/repositories/TroopRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 @Repository
 public interface TroopRepository extends JpaRepository<Troop, Long> {
   Troop findFirstByOrderByLevelDesc();
+
   List<Troop> findAllByKingdom(Kingdom kingdom);
 }
--- src/main/java/org/gfa/avustribesbackend/services/Troop/TroopService.java
+++ src/main/java/org/gfa/avustribesbackend/services/Troop/TroopService.java
@@ -4,6 +4,8 @@ import org.gfa.avustribesbackend.models.Kingdom;
 
 public interface TroopService {
   void creating(Kingdom kingdom);
+
   void eating();
+
   void upgrading(Kingdom kingdom);
 }
